2a10b49a68dc28a505c934a9e587a30e3d184712
sonhm2329 <hoangminhson29102001@gmail.com>
2023-08-28 12:15:28 +0700

    Xong record audio + video

--- app/src/main/java/com/shariar99/screenrecorder/MainActivity.java
+++ app/src/main/java/com/shariar99/screenrecorder/MainActivity.java
@@ -1,9 +1,14 @@
 package com.shariar99.screenrecorder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.hardware.display.DisplayManager;
 import android.hardware.display.VirtualDisplay;
 import android.media.MediaRecorder;
@@ -33,83 +38,71 @@ public class MainActivity extends AppCompatActivity {
     private MediaRecorder mediaRecorder;
     private Button startBtn;
     private Button stopBtn;
+    private static final int AUDIO_PERMISSION_REQUEST_CODE = 1001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Initialize MediaRecorder
+        // Initialize MediaRecorder
         mediaRecorder = new MediaRecorder();
 
-        //Get references to UI elements
+        // Get references to UI elements
         startBtn = findViewById(R.id.btn_start_recording);
         stopBtn = findViewById(R.id.btn_stop_recording);
 
+        // Get screen density
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        screenDensity = metrics.densityDpi;
+
+        // Initialize media projection manager
+        mediaProjectionManager = (MediaProjectionManager) getSystemService(Context.MEDIA_PROJECTION_SERVICE);
 
         startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startRecording();
+                requestRecordingPermissionsAndStart();
             }
         });
+
         stopBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 stopRecording();
             }
         });
-
-        //get screen density
-        DisplayMetrics metrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(metrics);
-        screenDensity = metrics.densityDpi;
-
-        //initialize media projection manager
-        mediaProjectionManager = (MediaProjectionManager) getSystemService(Context.MEDIA_PROJECTION_SERVICE);
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode != REQUEST_CODE) {
-            Log.e("MainActivity", "Unknown request code: " + requestCode);
-            return;
+    private void requestRecordingPermissionsAndStart() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
+            Log.i("DEBUGGGGGG", "vào");
+            startRecording();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, AUDIO_PERMISSION_REQUEST_CODE);
         }
-        if (resultCode != RESULT_OK) {
-            Toast.makeText(this,
-                    "Screen Cast Permission Denied", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        mediaProjection = mediaProjectionManager.getMediaProjection(resultCode, data);
-        virtualDisplay = createVirtualDisplay();
-        mediaRecorder.start();
     }
 
-    private VirtualDisplay createVirtualDisplay() {
-        return mediaProjection.createVirtualDisplay("MainActivity",
-                720,1280, screenDensity,
-                    DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
-                    mediaRecorder.getSurface(), null /*Callbacks*/, null
-                    /*Handler*/);
-        }
     private void startRecording() {
         startBtn.setEnabled(false);
         stopBtn.setEnabled(true);
+
+        releaseMediaRecorder();
+
         try {
             LocalDateTime now = LocalDateTime.now();
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH:mm:ss");
             String timestamp = now.format(formatter);
-//            String videoSavePath = Environment.getExternalStorageDirectory() + "/record_" + timestamp +".mp4";
             File appSpecificDirectory = getExternalFilesDir(null);
             String videoSavePath = new File(appSpecificDirectory, "record_" + timestamp + ".mp4").getAbsolutePath();
 
             // Configure the MediaRecorder settings
-//            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
             mediaRecorder.setVideoSource(MediaRecorder.VideoSource.SURFACE);
             mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
             mediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
-//            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
             mediaRecorder.setVideoEncodingBitRate(512 * 1000);
             mediaRecorder.setVideoFrameRate(30);
             mediaRecorder.setVideoSize(720, 1280);
@@ -138,9 +131,58 @@ public class MainActivity extends AppCompatActivity {
         mediaRecorder.stop();
         mediaRecorder.reset();
         mediaProjection.stop();
+        mediaProjection = null;
 
-        // Release the media recorder
-        mediaRecorder.release();
+        virtualDisplay.release();
+    }
+
+    private void releaseMediaRecorder() {
+        if (mediaRecorder != null) {
+            mediaRecorder.reset();
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == AUDIO_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Audio permission granted, you can start recording
+                startRecording();
+            } else {
+                // Audio permission denied, handle accordingly
+                Toast.makeText(this, "Audio permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode != REQUEST_CODE) {
+            Log.e("MainActivity", "Unknown request code: " + requestCode);
+            return;
+        }
+        if (resultCode != RESULT_OK) {
+            startBtn.setEnabled(true);
+            stopBtn.setEnabled(false);
+            Toast.makeText(this,
+                    "Screen Cast Permission Denied", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        mediaProjection = mediaProjectionManager.getMediaProjection(resultCode, data);
+        virtualDisplay = createVirtualDisplay();
+        mediaRecorder.start();
+    }
+
+    private VirtualDisplay createVirtualDisplay() {
+        return mediaProjection.createVirtualDisplay("MainActivity",
+                720,1280, screenDensity,
+                DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+                mediaRecorder.getSurface(), null /*Callbacks*/, null
+                /*Handler*/);
     }
 }
 
